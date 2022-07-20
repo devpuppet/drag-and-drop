5ed48dc4eae79bfd971faeb42c8777bdb40b23d8
Kamil <qkiz288@gmail.com>
2022-07-20 10:33:52 +0200

    Fixed duplication issue

--- src/project-list.ts
+++ src/project-list.ts
@@ -1,4 +1,4 @@
-import { Project } from "./models/project";
+import { Project, ProjectStatus } from "./models/project";
 import { projectState } from "./state";
 
 export class ProjectList {
@@ -17,7 +17,13 @@ export class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
 
@@ -27,6 +33,7 @@ export class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
