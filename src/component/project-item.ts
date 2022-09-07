058613cdc2889da5964251fccd7c38c74ef40c99
Kamil <qkiz288@gmail.com>
2022-09-07 08:17:51 +0200

    Added getter

--- src/component/project-item.ts
+++ src/component/project-item.ts
@@ -3,6 +3,10 @@ import { Component } from "../component/component-base";
 
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
+    get persons() {
+        return this.project.people === 1 ? '1 person' : `${this.project.people} persons`
+    }
+
     constructor(hostId: string, private project: Project) {
         super('single-project', hostId, false, project.id);
 
@@ -14,7 +18,7 @@ export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
